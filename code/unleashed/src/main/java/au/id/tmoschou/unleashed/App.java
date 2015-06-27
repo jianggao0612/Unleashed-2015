9ab1c5a473595a05d8035009f396be814d1b45eb
Terry Moschou <terry.moschou@d2dcrc.com.au>
2015-06-27 15:20:18 +0930

    added project skeleton

---
+++ code/unleashed/src/main/java/au/id/tmoschou/unleashed/App.java
@@ -0,0 +1,13 @@
+package au.id.tmoschou.unleashed;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
